f4e17779af191c0d3dfb0c59674dfdf9c1386523
juanignacio87 <97361082+juanignacio87@users.noreply.github.com>
2022-10-09 18:02:59 -0300

    Agregar archivos de proyecto.

---
+++ Clases/Parametro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlMoney.Clases
+{
+    internal class Parametro
+    {
+        public string Nombre { get; set; }
+
+        public object Valor { get; set; }
+
+        public bool Salida { get; set; }
+
+        public Parametro (string nombre, object valor)
+        {
+            Nombre = nombre;
+            Valor = valor;
+            Salida = false;
+        }
+
+        public Parametro(string nombre)
+        {
+            Nombre = nombre;
+            Salida = true;
+        }
+    }
+}
